b7bd6d42b0d4602e2fa56588802767d878484b60
ksenialukova <ksushalukova778@gmail.com>
2020-05-04 00:17:16 +0300

    Add tables for costs and forecasts

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -25,13 +25,40 @@ CREATE TABLE balance
 (
     date date NOT NULL,
     code INTEGER NOT NULL REFERENCES cash_entity (ce_code),
-    percent DECIMAL NOT NULL,
+    percent DECIMAL NOT NULL CHECK ( percent >= 0 ),
     PRIMARY KEY (date, code)
 );
 
 COMMENT ON TABLE balance IS 'Balance';
 
+CREATE TABLE cost
+(
+    from_code INTEGER NOT NULL REFERENCES cash_entity (ce_code),
+    to_code INTEGER NOT NULL REFERENCES cash_entity (ce_code),
+    distance INTEGER NOT NULL,
+    duration INTEGER NOT NULL,
+    PRIMARY KEY (from_code, to_code)
+);
+
+CREATE TABLE forecast_balance
+(
+    date date NOT NULL,
+    code INTEGER NOT NULL REFERENCES cash_entity (ce_code),
+    percent DECIMAL NOT NULL CHECK ( percent >= 0 ),
+    PRIMARY KEY (date, code)
+);
+
+CREATE TABLE forecast_entity_shipment
+(
+    ce_code INTEGER NOT NULL PRIMARY KEY,
+    last_date date NOT NULL,
+    first_next_date date NOT NULL,
+    second_next_date date,
+    days_for_filling INTEGER[]
+);
+
 
 DROP TABLE cash_entity CASCADE;
 DROP TABLE shipments;
 DROP TABLE balance;
+DROP TABLE forecast_entity_shipment;
